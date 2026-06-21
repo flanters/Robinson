ced55b6311c6baa60d595a23a971e4475e8c9489
flanters <flantersig@gmail.com>
2026-06-21 15:09:06 -0300

    Add files via upload

---
+++ questao2robinson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== SIMULAÇÃO DE PAGAMENTO PIX ===");
+        PagamentoPix pgPix = new PagamentoPix();
+        pgPix.ReceberDadosPix();
+        Console.Write("Processando: ");
+        pgPix.ProcessarPagamento();
+
+        Console.WriteLine("\n=== SIMULAÇÃO DE PAGAMENTO CARTÃO ===");
+        PagamentoCartaoCredito pgCartao = new PagamentoCartaoCredito();
+        pgCartao.ReceberDadosCartao();
+        Console.Write("Processando: ");
+        pgCartao.ProcessarPagamento();
+
+        Console.WriteLine("\n=========================================");
+        Console.WriteLine("          RESUMO DOS PAGAMENTOS          ");
+        Console.WriteLine("=========================================");
+        pgPix.MostrarPagamento();
+        pgCartao.MostrarPagamento();
+
+        Console.ReadKey();
+    }
+}
+
+
