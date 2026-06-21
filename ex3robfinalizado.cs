270d72fce8ca4ec1be69f1bfb23626b7c47d9a4b
flanters <flantersig@gmail.com>
2026-06-21 15:16:35 -0300

    Add files via upload

---
+++ ex3robfinalizado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== CADASTRO - CARRO ===");
+        Carro carro = new Carro();
+        carro.ReceberDados();
+
+        Console.WriteLine("\n=== CADASTRO - MOTO ===");
+        Moto moto = new Moto();
+        moto.ReceberDados();
+
+        Console.WriteLine("\n=== CADASTRO - CAMINHÃO ===");
+        Caminhao caminhao = new Caminhao();
+        caminhao.ReceberDados();
+
+        Console.WriteLine("\n=========================================");
+        Console.WriteLine("          RELATÓRIO DE MANUTENÇÃO        ");
+        Console.WriteLine("=========================================");
+
+        // O polimorfismo acontece aqui ao exibir os dados e calcular as taxas de cada um
+        carro.MostrarDados();
+        moto.MostrarDados();
+        caminhao.MostrarDados();
+
+        Console.ReadKey();
+    }
+}
+
